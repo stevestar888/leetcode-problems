a38c83da3cf18b83949b9e7bd2dc4d1b19c4b01e
Steven Xu <weareallhalfminotaur@gmail.com>
2020-03-24 17:11:02 -0500

    Create 118-pascal_triangle.py

---
+++ 118-pascal_triangle.py
@@ -0,0 +1,40 @@
+import pprint
+
+class Solution(object):
+    def generate(self, num_rows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        result = []
+        DEFAULT_ROW = [1]
+
+        #check if 0 rows, else give us the basic row to start off
+        if num_rows == 0:
+            return result
+        else:
+            result.append([1])
+
+        #num_rows - 1 because we only start summing when rows ≥ 2
+        for i in range(num_rows - 1):
+            prev_row = result[i]
+            new_row = [1] #the leading [1]
+
+            #len(prev_row) - 1 because we look at prev_row[j + 1]
+            for j in range(len(prev_row) - 1):
+                sum = prev_row[j] + prev_row[j + 1]
+                new_row.append(sum)
+
+            new_row.append(1) #the trailing [1]
+            result.append(new_row)
+
+        return result
+
+
+
+obj = Solution.generate(None, 12)
+pprint.pprint(obj)
+
+# Best Stats  
+# Runtime: 12 ms, faster than 93.27% of Python online submissions for Pascal's Triangle.
+# Memory Usage: 11.8 MB, less than 50.00% of Python online submissions for Pascal's Triangle.
