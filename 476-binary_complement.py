1c463cb95a4ed4d2c734b0142d0398aaae05597a
Steven Xu <weareallhalfminotaur@gmail.com>
2020-03-26 23:00:39 -0500

    Create 476-binary_complement.py

---
+++ 476-binary_complement.py
@@ -0,0 +1,46 @@
+"""
+Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
+
+My approach:
+    Generate the complement's binary string using num. Convert that binary string back into an int.
+Stats:
+    Runtime: 16 ms, faster than 74.90% of Python online submissions for Number Complement.
+    Memory Usage: 12.7 MB, less than 14.29% of Python online submissions for Number Complement.
+"""
+
+
+class Solution(object):
+    def findComplement(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        #iterate & generate binary string
+        flippedBin = ""
+        while (num > 0):
+            remainder = num % 2
+
+            #check remainder
+            if remainder == 0: #divided in whole
+                flippedBin = "1" + flippedBin #if converting to binary normally would add 0
+            else: #remainder is 1
+                flippedBin = "0" + flippedBin
+
+            num = num >> 1 #divide by 2
+
+
+        #convert the flippedBinary into int representation
+        result = 0
+        index = 0
+        while (flippedBin): #has char left
+            bit = flippedBin[-1] #get last bit
+
+            if bit == '1':
+                result += (1 << index) # multiply by 2
+            flippedBin = flippedBin[:-1] #truncate
+            index += 1 #increase power of 2
+        
+        return result
+
+a = Solution.findComplement(None, 5)
+print(a)
