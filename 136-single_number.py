b90ca7e56b69db8f49873765a5a984f91e5a3cb6
Steven Xu <weareallhalfminotaur@gmail.com>
2020-04-04 14:19:18 -0500

    Create 136-single_number.py

---
+++ 136-single_number.py
@@ -0,0 +1,21 @@
+"""
+Runtime: 64 ms, faster than 94.82% of Python online submissions for Single Number.
+Memory Usage: 15.2 MB, less than 5.40% of Python online submissions for Single Number.
+"""
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        set = {}
+        for num in nums:
+            if num in set:
+                set[num] = 1
+            else:
+                set[num] = 0
+        
+        for num in set:
+            if set[num] == 0:
+                return num
