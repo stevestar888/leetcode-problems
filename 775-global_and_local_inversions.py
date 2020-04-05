74ef0a2cc9140669d526be7518d4d79f0f5ec67d
Steven Xu <weareallhalfminotaur@gmail.com>
2020-04-05 17:31:29 -0500

    Create 775-global_and_local_inversions.py

---
+++ 775-global_and_local_inversions.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def isIdealPermutation(self, A):
+        """
+        :type A: List[int]
+        :rtype: bool
+        """
+        def localCount():
+            inv_count = 0
+            for i in range(len(A) - 1):
+                if A[i] > A[i + 1]:
+                    inv_count += 1
+            return inv_count
+
+        # modification of MergeSort
+        def globalCount(A):
+            #basecase
+            if len(A) == 1:
+                return 0
+            
+            #find left, right, and straddle
+            halfway = len(A) // 2
+            left = A[:halfway]
+            right = A[halfway:]
+
+            straddle_count = 0
+
+            for l in left:
+                for r in right:
+                    if l > r:
+                        straddle_count += 1
+
+            return globalCount(left) + globalCount(right) + straddle_count
+
+        # return localCount()
+        return globalCount(A) == localCount()
+
+
+A = [1, 2, 3, 4]
+A = [12, 3, 4, 9, 8, 22]
+# A = [1,0,2] #true
+# A = [1,2,0] #false
+
+obj = Solution.isIdealPermutation(None, A)
+print(obj)
