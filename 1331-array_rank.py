56f8b725d00e6c5c6789f412d66e0f125b20611c
Steven Xu <weareallhalfminotaur@gmail.com>
2020-05-10 16:05:06 -0500

    array rank

---
+++ 1331-array_rank.py
@@ -0,0 +1,35 @@
+"""
+Given an array of integers arr, replace each element with its rank.
+"""
+
+def arrayRankTransform(self, arr):
+    """
+    :type arr: List[int]
+    :rtype: List[int]
+    """
+    
+    results = arr[:]
+
+    # sort results
+    sorted_arr = sorted(arr[:])
+
+    rank = 0
+    prev_elem = None
+    for i in range(len(arr)):
+        elem = sorted_arr[i]
+
+        if elem != prev_elem:
+            rank += 1
+
+        elem_index = arr.index(elem)
+
+        print("element", elem)
+        print("index", elem_index)
+        print("rank", rank)
+        print("")
+
+        results[elem_index] = rank
+        arr[elem_index] = None
+        prev_elem = elem
+        
+        return(results)
