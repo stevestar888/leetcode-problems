1b4728dcf094bff6fa8734c8fc61be1c662e89c2
Steven Xu <weareallhalfminotaur@gmail.com>
2020-05-12 11:30:01 -0500

    Update 199-binary_tree_right_side_view.py

--- 199-binary_tree_right_side_view.py
+++ 199-binary_tree_right_side_view.py
@@ -1,16 +1,14 @@
 """
-Given a binary tree, imagine yourself standing on the right side of it, 
-return the values of the nodes you can see ordered from top to bottom.
+https://leetcode.com/problems/binary-tree-right-side-view/submissions/
 
-Stategy: use BFS to find when we have reached the end of a level (this is 
-what the inner for loop does). The last item at a given level is therefore
-the rightmost element.
+Stategy: use BFS to find when we have reached the end of a level (this is what the inner for loop does). 
+    The last item at a given level is therefore the rightmost element. 
+    Alternatively, you can use a counter variable to keep track of when you'll be on the last elem
+    of the level, then decrement at the end of each while loop iteration.
 
-Runtime:
+Runtime: O(V) or O(n) because it's a modification of BFS
     Runtime: 12 ms, faster than 97.69% of Python online submissions for Binary Tree Right Side View.
     Memory Usage: 12.9 MB, less than 7.14% of Python online submissions for Binary Tree Right Side View.
-    Runs in O(V) or O(n) because it's a modification of BFS
-
 """
 
 
@@ -20,6 +18,8 @@ Runtime:
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
+
 class Solution(object):
     def rightSideView(self, root):
         """
@@ -27,18 +27,16 @@ class Solution(object):
         :rtype: List[int]
         """
         ans = []
+        queue = deque()
         
         if (root == None):
             return ans
         
-        queue = []
         queue.append(root)
-        
-        while (queue): #have items in the bruce
+        while queue: #have items in the bruce
             # for item in queue:
-            for i in range(len(queue)):
-                item = queue[0]
-                del queue[0]
+            for i in xrange(len(queue)):
+                item = queue.popleft()
             
                 #check if we have left kid
                 if (item.left):
@@ -51,6 +49,45 @@ class Solution(object):
             ans.append(item.val)
             print(item.val)
         
+        return ans
+
+    # no inner BFS for-loop; use counter var instead
+    def rightSideView(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        ans = []
+        queue = deque()
+        visited = []
+        
+        #if graph is empty
+        if (root == None):
+            return ans
+        
+        #initiate BFS
+        queue.append(root)
+        counter = len(queue) #keeps track of what level we're on
+        
+        #begin BFS
+        while queue:
+            node = queue.popleft()
+            visited.append(node.val)
+
+            if node.left:
+                queue.append(node.left)
+
+            if node.right:
+                queue.append(node.right)
+            
+            # if we reached the end of a level, add that level's last node to ans
+            # else, reduce the counter by one
+            if counter > 0:
+                counter -= 1
+            if counter == 0: 
+                ans.append(visited[-1])
+                counter = len(queue)
+                
         return ans
             
             
