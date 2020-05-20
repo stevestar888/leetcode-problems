9d8ca8ea550aa65e0e8e15ac264e1a45e4ba19e3
Steven Xu <weareallhalfminotaur@gmail.com>
2020-05-19 22:34:06 -0500

    spelling

--- 49-group_anagrams.py
+++ 49-group_anagrams.py
@@ -2,12 +2,15 @@
 https://leetcode.com/problems/group-anagrams/
 
 Strat: 
-    We will use a dictionary, using each anagram group's letter occurance as a key.
-    To make things simplier, we can make a list of letter occurances, and then 
-    caste that to a string. For example, "abe" becomes [a: 1, b: 1, e: 1], aka
-    [1, 1, 0, 0, 1, 0, 0, ...]. At the end, the keys in the dictionary 
+    We will use a dictionary to store groups, using each anagram group's letter occurrence as a key.
+    There's not a simple way to store occurrences, but to make things simpler, we can make a list, then 
+    caste that to a string. For example, "abe" becomes [a: 1, b: 1, e: 1], aka [1, 1, 0, 0, 1, 0, 0, ...],
+	where index 1 is a, index 2 is b, index 3 is c, etc. At the end, the values in the dictionary are our groups. 
     
-Runtime: O(n+m) time, O(n) space
+Runtime: O(n\*m) time, O(n+m) space
+   The actual runtimes aren't phenomenal, but it's good in terms of Big Oh. Essentially, we run through 
+   every word in strs (which takes O(n) time) and we generate an occurrence list for every char (max of 
+   O(m) time. All together, that is O(n\*m) time.
     Runtime: 136 ms, faster than 41.31% of Python online submissions for Group Anagrams.
     Memory Usage: 16.7 MB, less than 14.58% of Python online submissions for Group Anagrams.
 """
