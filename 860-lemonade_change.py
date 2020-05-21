3135735e504b1a9e265a270d5ff72f2a0fd45f2d
Steven Xu <weareallhalfminotaur@gmail.com>
2020-05-20 22:12:10 -0500

    Create 860-lemonade_change.py

---
+++ 860-lemonade_change.py
@@ -0,0 +1,46 @@
+"""
+https://leetcode.com/problems/lemonade-change/submissions/
+
+Strat: Use a greedy algorithm (when a person hands us a $20, always try to give them a 
+    $10 + $5). The case for getting a $5 or $10 is straightforward.
+
+Runtime: O(n) time, O(1) space
+    Runtime: 152 ms, faster than 24.25% of Python online submissions for Lemonade Change.
+    Memory Usage: 12.9 MB, less than 33.33% of Python online submissions for Lemonade Change.
+"""
+
+class Solution(object):
+    def lemonadeChange(self, bills):
+        """
+        :type bills: List[int]
+        :rtype: bool
+        """
+        fives = tens = 0
+        #no need to keep track of twenties, bc that can't make change
+        
+        for bill in bills:
+            print(bill)
+            if bill == 5:
+                fives += 1
+                #and don't need to make change
+                
+            if bill == 10: 
+                tens += 1
+                #now make change with fives
+                if fives > 0:
+                    fives -= 1
+                else:
+                    return False
+                
+            if bill == 20:
+                #make change: with $5 + $5 + $5 or $10 + $5
+                if tens > 0 and fives > 0:
+                    tens -= 1
+                    fives -= 1
+                elif fives > 2:
+                    fives -= 3
+                else: 
+                    return False
+            
+        return True
+                    
