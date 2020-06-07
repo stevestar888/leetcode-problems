c38eb7518721ab1037b69768379eee745e8d0b13
Steven Xu <weareallhalfminotaur@gmail.com>
2020-06-07 18:20:15 -0500

    Update 938-range_sum_of_BST.py

--- 938-range_sum_of_BST.py
+++ 938-range_sum_of_BST.py
@@ -1,5 +1,16 @@
 """
 https://leetcode.com/problems/range-sum-of-bst/
+
+Runtime: O(n) time
+    Runtime: 284 ms, faster than 48.88% of Python online submissions for Range Sum of BST.
+    Memory Usage: 29.2 MB, less than 18.86% of Python online submissions for Range Sum of BST.
+    
+Strat: Traverse the tree while maintaining a running sum.
+
+Optimization: we can take advantage of the pure structure of the BST: all right children are bigger, 
+    all left children are smaller. When given a node, if its value is great than R (the upper limit),
+    then we don't need to check the right child anymore. The same logic can be applied to the 
+    bottom limit of L. This trick is applied to iterative solution.
 """
 
 # Definition for a binary tree node.
@@ -9,6 +20,8 @@ https://leetcode.com/problems/range-sum-of-bst/
 #         self.left = left
 #         self.right = right
 class Solution(object):
+    
+    #recursive (not optimized)
     def rangeSumBST(self, root, L, R):
         """
         :type root: TreeNode
@@ -28,7 +41,8 @@ class Solution(object):
         
         return root_val + left_val + right_val
     
-    
+
+    #iterative with optimization 
     def rangeSumBST(self, root, L, R):
         """
         :type root: TreeNode
@@ -45,18 +59,21 @@ class Solution(object):
             
             for _ in range(length):
                 item = stack.pop()
-                print(item.val)
+                
                 if L <= item.val <= R:
                     result += item.val
                     
-                if item.left:
-                    stack.append(item.left)
-                    
-                if item.right:
+                #the right child of the item node will be bigger than the item
+                if item.val < R and item.right: 
                     stack.append(item.right)
-                    
+                
+                #same logic as ^
+                if item.val > L and item.left:
+                    stack.append(item.left)
+                
         return result
-        
+    
+    #iterative (not optimized)
 #     def rangeSumBST(self, root, L, R):
 #         """
 #         :type root: TreeNode
@@ -73,15 +90,14 @@ class Solution(object):
             
 #             for _ in range(length):
 #                 item = stack.pop()
-                
+#                 print(item.val)
 #                 if L <= item.val <= R:
 #                     result += item.val
-#                 elif item.val < R and item.right: 
-#                     stack.append(item.right)
-#                 elif item.val > L and item.left:
+                    
+#                 if item.left:
 #                     stack.append(item.left)
-                
-#                 #the right child of the item node will be bigger than the item
-#                 #checking its value 
+                    
+#                 if item.right:
+#                     stack.append(item.right)
                     
 #         return result
