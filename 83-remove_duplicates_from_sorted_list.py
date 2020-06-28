e9554b3579a8f858b2ce18a8c737fb2d4098d116
Steven Xu <weareallhalfminotaur@gmail.com>
2020-06-28 12:24:06 -0500

    Update 83-remove_duplicates_from_sorted_list.py

--- 83-remove_duplicates_from_sorted_list.py
+++ 83-remove_duplicates_from_sorted_list.py
@@ -22,3 +22,28 @@ class Solution(object):
                 curr = curr.next
 
         return head
+
+#Starter code for generalizing to un-sorted list
+# class LLNode:
+#     def __init__(self, val, next=None):
+#         self.val = val
+#         self.next = next
+# class Solution:
+#     def solve(self, node):
+#         head = node
+#         seen = set()
+#         seen.add(None)
+        
+#         while node and node.next:
+#             print(node.val)
+#             if node.next.val in seen:
+#                 node.next = node.next.next
+#             else:
+#                 seen.add(node.val)
+#                 # prev = node
+#                 node = node.next
+            
+#         if node.next in seen:
+#             node = None
+    
+#         return head
