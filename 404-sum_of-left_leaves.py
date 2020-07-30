3242c714e24beeac69d0b24b2541a4e93a0ac1c5
Steven Xu <30362574+stevestar888@users.noreply.github.com>
2020-07-29 23:01:59 -0500

    Create 404-sum_of-left_leaves.py

---
+++ 404-sum_of-left_leaves.py
@@ -0,0 +1,50 @@
+"""
+https://leetcode.com/problems/sum-of-left-leaves/submissions/
+
+Strat:
+    I found the recursive solution to be the most palatable.
+"""
+class Solution(object):
+    def sumOfLeftLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        def dfs(node, is_left_branch):
+            if node == None:
+                return 0
+
+            #see if we are a 'leaf'
+            if node.left == None and node.right == None:
+                if is_left_branch: #we are at a left leaf
+                    return node.val
+                else: #at a right leaf
+                    return 0
+            else: #not at leaf, keep recursing
+                left = dfs(node.left, True)
+                right = dfs(node.right, False)
+
+                return left + right
+            
+        #--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#
+        return dfs(root, False)
+        
+    
+    """
+    Runtime: 28 ms, faster than 35.47% of Python online submissions for Sum of Left Leaves.
+    Memory Usage: 13.5 MB, less than 35.90% of Python online submissions for Sum of Left Leaves.
+    """
+    def sumOfLeftLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root == None:
+            return 0
+        
+        #if I have a left child and that child doesn't have kids, aka I have a left leaf
+        left_leaf_val = 0
+        if root.left and root.left.left == None and root.left.right == None:
+            left_leaf_val = root.left.val
+        
+        return left_leaf_val + self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
