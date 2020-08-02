b6fcb80a52f9d38f64e409ddf6ea84a5f246fdf1
Steven Xu <30362574+stevestar888@users.noreply.github.com>
2020-08-02 18:27:29 -0500

    Update 206-reverse_linked_list.py

--- 206-reverse_linked_list.py
+++ 206-reverse_linked_list.py
@@ -1,32 +1,56 @@
 """
-Reverse a singly linked list.
+https://leetcode.com/problems/reverse-linked-list/submissions/
 
-Runtime: 28 ms, faster than 40.74% of Python online submissions for Reverse Linked List.
-Memory Usage: 14.7 MB, less than 25.93% of Python online submissions for Reverse Linked List.
+Strat:
+    Iterative or recursive; two ways of recursion. Use _next instead of 
+    next, because next is a built-in function......
 """
-
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
 class Solution(object):
+    """
+    Iterative
+    
+    Stats: O(n) time, O(1) space -- traverse through all elements + keep only vars
+        Runtime: 28 ms, faster than 40.74% of Python online submissions for Reverse Linked List.
+        Memory Usage: 14.7 MB, less than 25.93% of Python online submissions for Reverse Linked List.
+    """
     def reverseList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
-        """        
-        prev_node = None
-        curr_node = head
+        """
+        curr = head
+        prev = None
         
-        while curr_node: #while our head points to something
-            next_node = curr_node.next
-            curr_node.next = prev_node
+        while curr:  #while we still point to something
+            _next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = _next
             
-            #advance to next node
-            prev_node = curr_node
-            curr_node = next_node
-            
-        #curr_node is None, so return prev_node
-        return prev_node
+        #curr is None, so return prev_node
+        return prev
+        
+        
+    """
+    Recursive (with helper), inspired from: https://leetcode.com/problems/reverse-linked-list/discuss/58127/Python-Iterative-and-Recursive-Solution
+    
+    Alternative recursion in Leetcode solution; decent vid on it: https://www.youtube.com/watch?v=MRe3UsRadKw
+    
+    Stats: O(n) time, O(n) space -- traverse through all elem + stack space
+        Runtime: 32 ms, faster than 38.40% of Python online submissions for Reverse Linked List.
+        Memory Usage: 19.3 MB, less than 5.34% of Python online submissions for Reverse Linked List.
+    """
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        return self._helper(head)
+        
+    def _helper(self, node, prev=None):
+        if node == None:
+            return prev
+        
+        _next = node.next
+        node.next = prev
+        return self._helper(_next, node)
