e13eaeadd90322304cd1ad94deb889d00c3c1218
Steven Xu <weareallhalfminotaur@gmail.com>
2020-08-27 09:33:41 -0500

    Update 1143-longest_common_subsequence.py

--- 1143-longest_common_subsequence.py
+++ 1143-longest_common_subsequence.py
@@ -6,6 +6,19 @@ Applications:
     checking the difference between two files (diffing). Git needs to do this 
     when merging branches. It's also used in genetic analysis (combined with 
     other algorithms) as a measure of similarity between two genetic codes."
+    
+Note:
+    Should be able to use the built-in cache instead of memoing
+    (only works in python3 ???). import supposed to be:
+    # from functools import lru_cache
+    
+    then
+    
+    @lru_cache(maxsize=None)
+    # def lcs(idx1, idx2):
+        ....
+    ^doesn't work, check https://stackoverflow.com/questions/45819563/python-importerror-name-lru-cache
+    
 """    
 class Solution(object):
     """
@@ -55,61 +68,16 @@ class Solution(object):
         
         #don't know why 2d arrays to store answers don't work:
         # memo = [[-1] * len(text2)] * len(text1)
-        # memo = [[-1] for _ in range(len2) for i in range(len1)]
+        # memo = [[-1] for _ in range(len2) for _ in range(len1)]
         #use a dictionary for now, which works just as well
         memo = {} 
         return lcs(0, 0)
     
     
     """
-    Use the built-in cache instead of memoing (only works in
-    python3 ???). import supposed to be:
-    # from functools import lru_cache
-    ^doesn't work, check https://stackoverflow.com/questions/45819563/python-importerror-name-lru-cache
-    
-    Stats: O(len(text1) * len(text2)) aka O(n * m) time
-    """
-#     def longestCommonSubsequence(self, text1, text2):
-#         """
-#         :type text1: str
-#         :type text2: str
-#         :rtype: int
-#         """        
-#         @lru_cache(maxsize=None)
-#         def lcs(idx1, idx2):
-#             """
-#             :type word1: str
-#             :type word2: str
-#             :rtype: int
-#             """
-#             #base cases (empty string)
-#             if idx1 == len(text1) or idx2 == len(text2):
-#                 return 0
-            
-#             #recursive cases
-#             answer = 0
-#             if text1[idx1] == text2[idx2]:
-#                 #if the char matches
-#                 answer = 1 + lcs(idx1 + 1, idx2 + 1)
-#             else: 
-#                 #if the chars don't match
-#                 call1 = lcs(idx1 + 1, idx2)
-#                 call2 = lcs(idx1, idx2 + 1)
-#                 answer = max(call1, call2)
-            
-#             #memoize the answer
-#             return answer
-        
-#         #--------------end helper function-------------- 
-        
-#         #create 2d array to store answers
-#         return lcs(0, 0)
-    
-    
-    """
     Bottom-up tabulation with 2d array
     
-    Stats: O(len(text1) * len(text2)) aka O(n * m) time
+    Stats: O(n * m) time, O(n * m) space
         Runtime: 356 ms, faster than 66.86% of Python online submissions for Longest Common Subsequence.
         Memory Usage: 21.5 MB, less than 26.95% of Python online submissions for Longest Common Subsequence.
     """
@@ -138,7 +106,9 @@ class Solution(object):
     """
     Bottom-up tabulation with 1d array
     
-    Stats: O(len(text1) * len(text2)) aka O(n * m) time
+    Inspired by: https://leetcode.com/problems/longest-common-subsequence/discuss/473089/Python-DP-with-T(n)-O(nm)-and-S(n)-O(m)
+    
+    Stats: O(n * m) time, O()
         Runtime: 300 ms, faster than 92.66% of Python online submissions for Longest Common Subsequence.
         Memory Usage: 12.7 MB, less than 97.13% of Python online submissions for Longest Common Subsequence.
     """
@@ -148,43 +118,27 @@ class Solution(object):
         :type text2: str
         :rtype: int
         """
+        #puts the shorter text in text2
+        if len(text1) < len(text2):
+            text1, text2 = text2, text1
+        
         len1 = len(text1) + 1
         len2 = len(text2) + 1
         memo = [0] * len2
         
         for i in range(1, len1):
-            prev_row_best = 0
+            prev_diagonal = 0
+
             for j in range(1, len2):
-                curr_row_best = memo[j]
+                curr = memo[j]
                 
                 if text1[i - 1] == text2[j - 1]:
-                    memo[j] = prev_row_best + 1
+                    memo[j] = prev_diagonal + 1
                 else:
                     memo[j] = max(memo[j - 1], memo[j])
             
-                prev_row_best = curr_row_best
+                prev_diagonal = curr
+            # print(memo)
             
         return memo[-1]
-        
-        
-#         len1 = len(text1) + 1
-#         len2 = len(text2) + 1
-#         # Use only 1D array for store result because we only need to get result from previous line and current line
-#         dp = [0] * len2
-        
-#         for i in range(1, len1):
-#             prev_diagonal = 0
-#             for j in range(1, len2):
-#                 temp = dp[j]
-                
-#                 if text1[i - 1] == text2[j - 1]:
-#                     dp[j] = prev_diagonal + 1
-#                 else:
-#                     # dp[j - 1]: left element, this has overriden
-#                     # dp[j]: top element because at this point top element hasn't overriden
-#                     dp[j] = max(dp[j - 1], dp[j])
-                
-#                 prev_diagonal = temp
-                
-#         return dp[len2 - 1]
     
