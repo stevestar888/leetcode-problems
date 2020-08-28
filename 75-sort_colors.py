3ed16dbcd31fd623cc689d025ed611a6f3e9d247
Steven Xu <weareallhalfminotaur@gmail.com>
2020-08-28 11:44:32 -0500

    Update 75-sort_colors.py

--- 75-sort_colors.py
+++ 75-sort_colors.py
@@ -3,8 +3,14 @@ class Solution(object):
     Two-pass algorithm using counting sort. First, iterate the array counting number of 0's, 1's, and 2's, 
     then overwrite array with total number of 0's, then 1's and followed by 2's.
     
+    TODO: can be done in place
+
     Runtime: 12 ms, faster than 99.10% of Python online submissions for Sort Colors.
     Memory Usage: 12.6 MB, less than 99.56% of Python online submissions for Sort Colors.
+
+    Note: problem comes from 
+        https://en.wikipedia.org/wiki/Dutch_national_flag_problem
+        Also see https://en.wikipedia.org/wiki/American_flag_sort
     """
     def sortColors(self, nums):
         """
