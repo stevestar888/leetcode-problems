e8c0666aaa268ab7940cbf36e56232e8baf5ca36
Steven Xu <weareallhalfminotaur@gmail.com>
2020-10-20 14:49:36 -0400

    Create 1624-largest_substring_between_two_equal_chars.py

---
+++ 1624-largest_substring_between_two_equal_chars.py
@@ -0,0 +1,27 @@
+"""
+https://leetcode.com/problems/largest-substring-between-two-equal-characters/
+
+Runtime: 24 ms, faster than 61.23% of Python online submissions for Largest Substring Between Two Equal Characters.
+Memory Usage: 13.3 MB, less than 55.07% of Python online submissions for Largest Substring Between Two Equal Characters.
+"""
+class Solution(object):
+    def maxLengthBetweenEqualCharacters(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        #store the index for each letter's occurence
+        first_occur = {}
+        
+        best = 0
+        
+        for i, char in enumerate(s):
+            found = first_occur.get(char, None)
+            print(i, found)
+            if found != None: #calc current substring length
+                best = max(best, i - found)
+            else:
+                first_occur[char] = i
+            
+        #subtract 1 to account for indexing
+        return best - 1
