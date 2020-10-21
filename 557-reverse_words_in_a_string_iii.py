dd3d04f94dd3929ec1ac8aece61d743af576f7ef
Steven Xu <weareallhalfminotaur@gmail.com>
2020-10-20 22:26:41 -0400

    Create 557-reverse_words_in_a_string_iii.py

---
+++ 557-reverse_words_in_a_string_iii.py
@@ -0,0 +1,40 @@
+"""
+https://leetcode.com/problems/reverse-words-in-a-string-iii/
+
+O(n) / linear time, O(n) / linear space
+"""
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        #make sure our string is not empty
+        if not s:
+            return ""
+        
+        s = list(s)
+        
+        word_index = 0 #keeps track of the start of a word
+        ptr1, ptr2, = 0, 0
+        while ptr2 < len(s):
+            #advance the pointers to the correct locations
+            if word_index < len(s):
+                ptr1 = ptr2 = word_index
+                while ptr2 + 1 < len(s) and s[ptr2 + 1] != " ":
+                    ptr2 += 1
+            else:
+                break
+            
+            #store where the next word begins (ptr2 points at the last char, so go
+            #2 chars after ptr2)
+            word_index = ptr2 + 2
+            
+            #begin swapping and close in
+            while ptr1 < ptr2:
+                s[ptr1], s[ptr2] = s[ptr2], s[ptr1]
+                ptr1 += 1
+                ptr2 -= 1
+            
+            # print("".join(s))
+        return "".join(s)
