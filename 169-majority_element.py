048feee1a4fe139d3b721aaa20e869b99b588b2b
Steven Xu <weareallhalfminotaur@gmail.com>
2020-10-27 01:11:58 -0400

    Create 169-majority_element.py

---
+++ 169-majority_element.py
@@ -0,0 +1,28 @@
+"""
+https://leetcode.com/problems/majority-element/
+
+Strat:
+    The majority element must occur at least n / 2 times.
+    Looking at index len(n) / 2 gives us the elem that 
+    is in the majority.
+    
+Stats: O(n lgn)
+
+Also an algo that is O(n) time O(1) space: 
+https://en.wikipedia.org/wiki/File:Boyer-Moore_MJRTY.svg
+    
+    #TODO 
+"""
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = sorted(nums)
+        mid = len(nums) / 2
+        
+        return nums[mid]
+        # or just:
+        # return sorted(nums)[len(nums) / 2]
+
