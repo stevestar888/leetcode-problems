1fb6aca8dd1d37771dfe23943c8301f3a619e64f
Steven Xu <weareallhalfminotaur@gmail.com>
2020-10-27 01:13:50 -0400

    Create 1150-check_if_number_is_majority_in_sorted_arr.py

---
+++ 1150-check_if_number_is_majority_in_sorted_arr.py
@@ -0,0 +1,20 @@
+"""
+https://leetcode.com/problems/check-if-a-number-is-majority-element-in-a-sorted-array/
+
+#TODO
+
+see https://leetcode.com/problems/check-if-a-number-is-majority-element-in-a-sorted-array/discuss/355470/Python%3A-Two-Ways-O(N)-and-O(logN)
+
+Majority element basket:
+https://leetcode.com/problems/majority-element/
+https://leetcode.com/problems/majority-element-ii/
+https://leetcode.com/problems/check-if-a-number-is-majority-element-in-a-sorted-array/
+"""
+
+class Solution(object):
+    def isMajorityElement(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
