fbd3ba6d7de2b9c652e0848749eb4582ebfe35b4
Steven Xu <weareallhalfminotaur@gmail.com>
2022-06-11 16:31:17 -0500

    Update 322-coin_change.py

--- 322-coin_change.py
+++ 322-coin_change.py
@@ -1,39 +1,42 @@
 """
-Time limit exceeded... but an attempt at top-down recursive
+Inspired by this really clean solution: 
+https://leetcode.com/problems/coin-change/discuss/1679136/Python-Simplest-Recursive-Solution-with-Memoization
+
+Strat:
+    Recursive DP w/ Memoization
+Stats:
+    Runtime: 2674 ms, faster than 20.15% of Python3 online submissions for Coin Change.
+    Memory Usage: 44.4 MB, less than 5.47% of Python3 online submissions for Coin Change.
 """
-class Solution(object):
-    def coinChange(self, coins, amount):
-        """
-        :type coins: List[int]
-        :type amount: int
-        :rtype: int
-        """
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
         memo = {}
         
-        def dp_helper(amount_left, coins_used):
+        def solve(amount_left):
+            #see if solution has already been calculated
+            if memo.get(amount_left):
+                return memo[amount_left]
+            
             #base cases
             if amount_left == 0:
-                return coins_used
+                return 0
             elif amount_left < 0:
-                return sys.maxint
-            
-            #see if solution has already been calculated
-            if memo.get((amount_left, coins_used), -1) > 0:
-                # print(memo.get((amount_left, coins_used)))
-                return memo.get((amount_left, coins_used))
+                return float('inf')
             
             #recursive cases
-            best = sys.maxint
+            best = float('inf')
             for coin in coins:
-                coins_needed = dp_helper(amount_left - coin, coins_used + 1)
+                coins_needed = 1 + solve(amount_left - coin)
                 best = min(best, coins_needed)
             
             #memoize the result for future use
-            memo[(amount_left, coins_used)] = best
+            memo[amount_left] = best
             return best
+        #--------------end helper function-------------- 
         
-        result = dp_helper(amount, 0)
-        if result == sys.maxint:
+        
+        result = solve(amount)
+        if result == float('inf'):
             return -1
         else:
             return result
